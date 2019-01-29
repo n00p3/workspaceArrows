f49550543087429b4027702924c4837d54b6c010
nn <mateusz.glowski.96@gmail.com>
2019-01-29 15:17:00 +0100

    Initial commit

---
+++ workspaceLeft/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace workspaceLeft
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        private static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+
+        private const int KEYEVENTF_EXTENDEDKEY = 1;
+        private const int KEYEVENTF_KEYUP = 2;
+
+        public static void KeyDown(Keys vKey)
+        {
+            keybd_event((byte)vKey, 0, KEYEVENTF_EXTENDEDKEY, 0);
+        }
+
+        public static void KeyUp(Keys vKey)
+        {
+            keybd_event((byte)vKey, 0, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+        }
+
+
+        static void Main(string[] args)
+        {
+            KeyDown(Keys.LWin);
+            KeyDown(Keys.LControlKey);
+            KeyDown(Keys.Left);
+
+            KeyUp(Keys.LWin);
+            KeyUp(Keys.LControlKey);
+            KeyUp(Keys.Left);
+        }
+    }
+}
